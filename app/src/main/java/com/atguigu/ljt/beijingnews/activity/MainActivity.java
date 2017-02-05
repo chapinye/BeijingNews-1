8b375002558e913d5d151eece5f3b07782965536
ljt474297694 <474297694@qq.com>
2017-02-05 16:46:36 +0800

    slidingMenu的使用步骤做主页面

--- app/src/main/java/com/atguigu/ljt/beijingnews/activity/MainActivity.java
+++ app/src/main/java/com/atguigu/ljt/beijingnews/activity/MainActivity.java
@@ -1,16 +1,24 @@
 package com.atguigu.ljt.beijingnews.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.atguigu.ljt.beijingnews.R;
+import com.atguigu.ljt.beijingnews.util.DensityUtil;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SlidingFragmentActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setBehindContentView(R.layout.leftmenu);
+
+        SlidingMenu slidingMenu = getSlidingMenu();
+        slidingMenu.setMode(SlidingMenu.LEFT);
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        slidingMenu.setBehindOffset(DensityUtil.dip2px(this, 220));
     }
 }
